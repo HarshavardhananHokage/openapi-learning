750bba000a47dfa4ccf79a2a3621b79955359738
Harshavardhanan <harshavardhan2990@gmail.com>
2025-10-09 22:05:23 +0530

    Hello World OpenAI

---
+++ helloworld/helloworld.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPEN_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+response = client.responses.create(model = "gpt-4o-mini", input = "Write me a 1000 word story on Demonte-The Rap God Dragon")
+
+print(response.output_text)
